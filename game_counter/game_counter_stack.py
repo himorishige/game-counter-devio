dcc00d6bcf09a6357d03342cfa35ea4d28dd8282
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-10-08 17:27:37 +0900

    add DynamoDB and 1stLambda

--- game_counter/game_counter_stack.py
+++ game_counter/game_counter_stack.py
@@ -1,5 +1,13 @@
-from aws_cdk import Stack
+from aws_cdk import (
+    Stack,
+    RemovalPolicy,
+    aws_dynamodb as dynamodb,
+    aws_lambda as lambda_,
+    aws_apigateway as apigw
+)
 from constructs import Construct
+import os
+
 
 class GameCounterStack(Stack):
 
@@ -7,3 +15,21 @@ class GameCounterStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        # DynamoDB ログデータ格納用
+        table = dynamodb.Table(self, "GameCounterTable",
+                               table_name="GameCounter",
+                               partition_key=dynamodb.Attribute(
+                                   name="username", type=dynamodb.AttributeType.STRING),
+                               sort_key=dynamodb.Attribute(
+                                   name="timestamp", type=dynamodb.AttributeType.NUMBER),
+                               billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+                               removal_policy=RemovalPolicy.DESTROY
+                               )
+
+        # Lambda ログデータ一覧表示用
+        list_data = lambda_.Function(self, "ListData", code=lambda_.Code.from_asset(
+            "lambda"), handler="counter.list_data", runtime=lambda_.Runtime.PYTHON_3_9,
+            environment={'TABLE_NAME': table.table_name})
+
+        table.grant_read_data(list_data)
