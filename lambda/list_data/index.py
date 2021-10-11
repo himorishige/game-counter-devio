2193277df3d415f6a043d329e2957197e9a81ad4
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2021-10-11 20:49:40 +0900

    update: lamnda functions

--- lambda/list_data/index.py
+++ lambda/list_data/index.py
@@ -15,17 +15,27 @@ headers = {
 def handler(event, context):
     print("Received event: " + json.dumps(event))
 
-    username = event["username"]
-    starttime = event["starttime"]
-    endtime = event["endtime"]
+    # クエリーパラメータの存在確認
+    if event.get("queryStringParameters"):
+        username = event['queryStringParameters'].get("username")
+        starttime = event['queryStringParameters'].get("starttime")
+        endtime = event['queryStringParameters'].get("endtime")
+    else:
+        username = None
 
     try:
-        response = table.query(
-            KeyConditionExpression=Key("username").eq(username) & Key(
-                "timestamp").between(starttime, endtime)
-        )
+        # usernameの指定がある場合はusernameの指定した期間の一覧を出力
+        if username:
+            response = table.query(
+                KeyConditionExpression=Key("username").eq(username) & Key(
+                    "timestamp").between(int(starttime), int(endtime))
+            )
+        # usernameの指定がない場合は全件一覧を出力
+        else:
+            response = table.scan()
+
         if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
-            raise Exception
+            raise Exception("Unable to connect to the database.")
         else:
             status_code = 200
             res = response["Items"]
